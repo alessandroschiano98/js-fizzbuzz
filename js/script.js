e2a0eb8185380beddc48dc08ee9965de40479c49
alessandroschiano98 <schianoski98@gmail.com>
2025-02-20 13:44:25 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 /* STAMPARE NUMERO DA 1 A 100*/
-for(let i = 1; i <= 100; i++){
-    console.log(i);
+
+for(i = 1; i <= 100; i++) {
+    if(i % 3 === 0) {
+        console.log("Fizz");
+    } else if(i % 5 === 0) {
+        console.log("Buzz");
+    } else if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else {
+        console.log(i);
+    }
 }
+
+
 
