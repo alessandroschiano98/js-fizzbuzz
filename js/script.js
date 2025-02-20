8f7f7486395abc71cccf20fd0e49dfa2f3f38f90
alessandroschiano98 <schianoski98@gmail.com>
2025-02-20 13:52:35 +0100

    completare parte finale

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-/* STAMPARE NUMERO DA 1 A 100*/
+/* STAMPARE NUMERO DA 1 A 100 + MULTIPLI DI 3 FIZZ + MULTIPLI DI 5 BUZZ + MULTIPLY DI 3 E 5 FIZZBUZZ*/
 
-for(i = 1; i <= 100; i++) {
-    if(i % 3 === 0) {
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
         console.log("Fizz");
-    } else if(i % 5 === 0) {
+    } else if (i % 5 === 0) {
         console.log("Buzz");
-    } else if (i % 3 === 0 && i % 5 === 0){
+    } else if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
     } else {
         console.log(i);
