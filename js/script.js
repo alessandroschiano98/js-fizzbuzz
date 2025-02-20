98eae6cec6041c03b4589b44ee27a5f5b3519f87
alessandroschiano98 <schianoski98@gmail.com>
2025-02-20 18:41:18 +0100

    esercizio terminato (corretto l'l'ordine di if e else if in modo tale da dare come condizione primaria quella dei 3 e dei 5 %

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-/* STAMPARE NUMERO DA 1 A 100 + MULTIPLI DI 3 FIZZ + MULTIPLI DI 5 BUZZ + MULTIPLY DI 3 E 5 FIZZBUZZ*/
+/* ESERCIZIO:
+STAMPARE NUMERO DA 1 A 100 + 
+MULTIPLI DI 3 sostituirli con FIZZ + 
+MULTIPLI DI 5 sostituirli con BUZZ + 
+MULTIPLY DI 3 E 5 sostituirli con FIZZBUZZ;
+*/
 
 
 for (i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        console.log("Fizz");
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log("fizz");
     } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+        console.log("buzz");
+    } else if (i % 3 === 0) {
+        console.log("fizzBuzz");
     } else {
         console.log(i);
     }
